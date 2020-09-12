acc012ad637bcd0516f67da4ba1fd2b71371b99f
amiralitaheri <amiralitaheri64@gmail.com>
2020-09-12 21:26:24 +0430

    added github link and padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,6 @@ import styles from './Footer.module.scss';
 
 export default () =>
     <footer className={styles.footer}>
-        <span>Available on <a href='https://amiralitaheri.ir' rel='noopener'>github</a></span>
+        <span>Available on <a href='https://github.com/amiralitaheri/spotify-data' rel='noopener'>github</a></span>
         <span>Made with <span role='img' aria-label='love'>❤️ </span>by <a href='https://amiralitaheri.ir' rel='noopener'>Amirali</a></span>
     </footer>
