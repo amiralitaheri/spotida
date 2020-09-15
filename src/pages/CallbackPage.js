2efa0aee870c4f0f513d6697a2fe503bc9b5af51
amiralitaheri <amiralitaheri64@gmail.com>
2020-09-15 22:43:56 +0430

    added some loading style

--- src/pages/CallbackPage.js
+++ src/pages/CallbackPage.js
@@ -2,6 +2,9 @@ import React, {useEffect} from "react";
 import {login, getData} from '../redux/actions';
 import {useDispatch, useSelector} from 'react-redux';
 import {Redirect} from "react-router";
+import Container from "../components/Container";
+import styles from './CallbackPage.module.scss';
+import Card from "../components/Card";
 
 export default (props) => {
     const isDataLoaded = useSelector(state => state.data.isDataLoaded);
@@ -18,5 +21,14 @@ export default (props) => {
         return <Redirect to="/dashboard"/>
     }
 
-    return <h1>Loading...</h1>
+    return <Container className={styles.container}>
+        <Card className={styles.card}>
+            <div className={styles.spinner}>
+                <span className={styles.bounce1}/>
+                <span className={styles.bounce2}/>
+                <span/>
+            </div>
+            <h5 className={styles.text}>Stealing your data <span role='img'>😎</span></h5>
+        </Card>
+    </Container>
 }
