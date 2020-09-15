9c8f24ef120a9a7897cd9cf2eda35aa211cb2c98
amiralitaheri <amiralitaheri64@gmail.com>
2020-09-15 12:26:54 +0430

    moved spotify.js to utils

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {FaSpotify} from 'react-icons/fa';
 import styles from './LoginButton.module.scss';
-import * as Spotify from '../spotify';
+import * as Spotify from '../utils/spotify';
 
 export default () =>
-    <button className={styles.loginButton} onClick={event=>{
+    <button className={styles.loginButton} onClick={event => {
         event.preventDefault();
         window.location.href = `https://accounts.spotify.com/authorize?client_id=${Spotify.clientId}&redirect_uri=${window.location}callback/&scope=${Spotify.scopes}&response_type=token`;
     }}>
