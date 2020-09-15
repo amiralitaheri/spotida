1f057d1365f616e94cc0c3edd9f4604b413403f9
amiralitaheri <amiralitaheri64@gmail.com>
2020-09-15 23:45:04 +0430

    added tooltip

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,6 +6,7 @@ import {RiPlayListFill} from 'react-icons/ri';
 import {useDispatch, useSelector} from "react-redux";
 import {changeTab, logout} from "../redux/actions";
 import {cn} from "../utils";
+import Tooltip from '@material-ui/core/Tooltip';
 
 export default () => {
     const dispatch = useDispatch();
@@ -14,23 +15,38 @@ export default () => {
     return <nav className={styles.nav}>
         <ul className={styles.ul}>
             <li id={cn(selectedTab === 'stats' && styles.selected)}>
-                <button className={styles.button} onClick={useCallback(() => dispatch(changeTab('stats')), [])}>
-                    <BiStats/></button>
+                <Tooltip title="Statistics" placement="right">
+                    <button className={styles.button} onClick={useCallback(() => dispatch(changeTab('stats')), [])}>
+                        <BiStats/>
+                    </button>
+                </Tooltip>
             </li>
             <li id={cn(selectedTab === 'playlist' && styles.selected)}>
-                <button className={styles.button} onClick={useCallback(() => dispatch(changeTab('playlist')), [])}>
-                    <RiPlayListFill/></button>
+                <Tooltip title="Playlist Creator" placement="right">
+                    <button className={styles.button} onClick={useCallback(() => dispatch(changeTab('playlist')), [])}>
+                        <RiPlayListFill/>
+                    </button>
+                </Tooltip>
             </li>
             <li id={cn(selectedTab === 'image' && styles.selected)}>
-                <button className={styles.button} onClick={useCallback(() => dispatch(changeTab('image')), [])}>
-                    <BiImage/></button>
+                <Tooltip title="Image Creator" placement="right">
+                    <button className={styles.button} onClick={useCallback(() => dispatch(changeTab('image')), [])}>
+                        <BiImage/>
+                    </button>
+                </Tooltip>
             </li>
             <li id={cn(selectedTab === 'help' && styles.selected)}>
-                <button className={styles.button} onClick={useCallback(() => dispatch(changeTab('help')), [])}>
-                    <FaHandsHelping/></button>
+                <Tooltip title="Help Us" placement="right">
+                    <button className={styles.button} onClick={useCallback(() => dispatch(changeTab('help')), [])}>
+                        <FaHandsHelping/>
+                    </button>
+                </Tooltip>
             </li>
         </ul>
-        <button className={cn(styles.logout, styles.button)} onClick={useCallback(() => dispatch(logout()), [])}>
-            <BiLogOut/></button>
+        <Tooltip title="Logout" placement="right">
+            <button className={cn(styles.logout, styles.button)} onClick={useCallback(() => dispatch(logout()), [])}>
+                <BiLogOut/>
+            </button>
+        </Tooltip>
     </nav>
 }
