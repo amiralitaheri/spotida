3cb2330bdff4a8bfd62ca05637336c64dfad0a8d
amiralitaheri <amiralitaheri64@gmail.com>
2020-09-16 12:53:43 +0430

    removed console log

--- src/utils/spotify.js
+++ src/utils/spotify.js
@@ -2,9 +2,6 @@ export const clientId = '75f5d4538fc14160802eaf7183a4d74e';
 export const scopes = ['user-top-read', 'playlist-modify-public'].join(' ');
 
 export const addTracksToPlaylist = async (token, uris, playlistId) => {
-    console.log(JSON.stringify({
-        'uris': uris.slice(0, 100).join(',')
-    }));
     const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
         headers: {
             'Authorization': `Bearer ${token}`,
