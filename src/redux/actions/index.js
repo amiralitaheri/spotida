df6e3f627fc7923b6f0ebfaa5394486aa5ddb07f
amiralitaheri <amiralitaheri64@gmail.com>
2020-09-17 18:10:07 +0430

    moved functions to util

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,3 +1,5 @@
+import {calculateTopGenres, getAverageFromAudioFeatures, getLeastPopular, getTrackIds} from "../../utils";
+
 export const login = (token) => ({
     type: 'LOGIN',
     payload: {
@@ -64,82 +66,6 @@ export const getData = () => (dispatch, getState) => {
     }).catch((err) => {
         console.log(err);
     });
-}
-
-const getLeastPopular = (list) => {
-    let result = null;
-    let resultPopularity = 100;
-    list.items.forEach(item => {
-        if (item.popularity < resultPopularity) {
-            resultPopularity = item.popularity;
-            result = item;
-        }
-    });
-    return result;
-};
-
-const getAverageFromAudioFeatures = (audioFeatures) => {
-    const average = {
-        danceability: 0.0,
-        energy: 0.0,
-        loudness: 0.0,
-        speechiness: 0.0,
-        acousticness: 0.0,
-        instrumentalness: 0.0,
-        liveness: 0.0,
-        valence: 0.0,
-        tempo: 0.0,
-        duration_ms: 0.0,
-    };
-    audioFeatures.forEach(af => {
-        Object.keys(average).forEach(key => {
-            average[key] += af[key]
-        })
-    })
-    Object.keys(average).forEach(key => {
-        average[key] /= audioFeatures.length;
-    })
-    return average;
-}
-const getTrackIds = (listTracks) => {
-    const result = new Set();
-    listTracks.forEach(list => {
-        list.items.forEach(track => {
-            result.add(track.id);
-        })
-    });
-    return Array.from(result);
-}
-const calculateTopGenres = (listArtists) => {
-    const topGenres = {}
-    listArtists.forEach(list => {
-        list.items.forEach(artist => {
-            artist.genres.forEach(genre => {
-                if (genre in topGenres) {
-                    topGenres[genre] += 1;
-                } else {
-                    topGenres[genre] = 1;
-                }
-            })
-        })
-    });
-    const items = Object.keys(topGenres).map(function (key) {
-        return [key, topGenres[key]];
-    });
-    items.sort(function (first, second) {
-        return second[1] - first[1];
-    });
-    const result = []
-    const others = ['others', 0]
-    items.forEach(item => {
-        if (item[1] > 10) {
-            result.push(item);
-        } else {
-            others[1] += item[1];
-        }
-    })
-    result.push(others)
-    return result;
 }
 
 export const changeTab = (tabName) => ({
