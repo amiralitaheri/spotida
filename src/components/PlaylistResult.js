f421ba894620ef4718edf75c1035afe9b612382d
amiralitaheri <amiralitaheri64@gmail.com>
2020-09-18 23:23:22 +0430

    fixed playlist result width problem

--- src/components/PlaylistResult.js
+++ src/components/PlaylistResult.js
@@ -8,7 +8,7 @@ export default (props) => {
             return <span className={cn(styles.result, styles.pending)}>Wait a second</span>
         case 'SUCCESS':
             return <span className={cn(styles.result, styles.success)}>Created successfully! <a
-                href={props.message}>{props.message}</a></span>
+                href={props.message}>Click to open playlist</a></span>
         case 'ERROR':
             return <span className={cn(styles.result, styles.error)}>{props.message}</span>
         default:
