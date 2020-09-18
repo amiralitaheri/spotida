c811a65321fcf966afda795627627f7be3951373
amiralitaheri <amiralitaheri64@gmail.com>
2020-09-18 21:06:25 +0430

    changed h1 font

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -16,7 +16,7 @@ export default () => {
     return <Container className={styles.container}>
         <main className={styles.main}>
             <Card className={styles.card}>
-                <div className={styles.logo}><h1>Spotida</h1></div>
+                <div className={styles.logo}><h1><span>S</span>poti<span>da</span></h1></div>
                 <p>As recently I have noticed that Spotify.me no longer works <span aria-label='sad'
                                                                                     role='img'>😭</span>.
                     I needed a place to see my Spotify statistics. So I have created this website and added some other
