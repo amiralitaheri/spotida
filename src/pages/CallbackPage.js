e3a2aefcae379f63603cfec1621330b5a4fdae6e
amiralitaheri <amiralitaheri64@gmail.com>
2020-10-07 14:50:29 +0330

    added something for access denied

--- src/pages/CallbackPage.js
+++ src/pages/CallbackPage.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {login, getData} from '../redux/actions';
 import {useDispatch, useSelector} from 'react-redux';
 import {Redirect} from "react-router";
@@ -7,21 +7,37 @@ import styles from './CallbackPage.module.scss';
 import Card from "../components/Card";
 import {parseUrl} from "../utils";
 import ErrorCard from "../components/ErrorCard";
+import LoginButton from "../components/LoginButton";
 
 export default (props) => {
     const isDataLoaded = useSelector(state => state.data.isDataLoaded);
     const dataLoadError = useSelector(state => state.data.dataLoadError);
     const dataLoadErrorMessage = useSelector(state => state.data.dataLoadErrorMessage);
     const dispatch = useDispatch();
+    const [accessDeniedError, setAccessDeniedError] = useState(false);
 
     useEffect(() => {
-        const urlParameters = parseUrl(props.location.hash);
-        dispatch(login(urlParameters['#access_token']));
-        dispatch(getData());
-    }, [dispatch, props.location.hash]);
+        const urlHashParameters = parseUrl(props.location.hash);
+        const urlQueryParameters = parseUrl(props.location.search);
+        if (urlQueryParameters['?error'] === 'access_denied') {
+            setAccessDeniedError(true);
+        } else {
+            dispatch(login(urlHashParameters['#access_token']));
+            dispatch(getData());
+        }
+    }, [dispatch, props.location]);
 
     if (isDataLoaded === true) {
         return <Redirect to="/dashboard"/>
+    }
+
+    if (accessDeniedError) {
+        return <Container className={styles.container}>
+            <Card className={styles.card}>
+                <h5 className={styles.text}>Access Denied!! <span aria-label='crying' role='img'>😭</span></h5>
+                <LoginButton/>
+            </Card>
+        </Container>
     }
 
     if (dataLoadError) {
